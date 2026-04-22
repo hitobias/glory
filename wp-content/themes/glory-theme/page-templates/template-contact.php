59be9367ee3ca7d187b7d4041248de262ec3f056
Tobias Pan <hitobias@tpech.org>
2026-04-22 08:41:45 +0800

    fix: repair broken Google Maps embed on contact page
    Replace truncated embed URL with address-based maps.google.com format
    that doesn't require API key and survives copy/paste without breakage.

--- wp-content/themes/glory-theme/page-templates/template-contact.php
+++ wp-content/themes/glory-theme/page-templates/template-contact.php
@@ -94,7 +94,7 @@ get_header();
                     <!-- Google Maps -->
                     <div class="rounded-2xl overflow-hidden shadow-card">
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7230.119059180714!2d121.55524444266273!3d25.032053761315913!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442abca1d187111%3A0xe2ecefefb4d53d65!2z5Y-w5YyX5biC5L-h576p5Y2A5L-h576p6Lev5Zub5q61NDYw6Jmf!5e0!3m2!1szh-TW!2stw!4v1"
+                            src="https://maps.google.com/maps?q=%E5%8F%B0%E5%8C%97%E5%B8%82%E4%BF%A1%E7%BE%A9%E8%B7%AF%E5%9B%9B%E6%AE%B5460%E8%99%9F3F-1&t=&z=16&ie=UTF8&iwloc=&output=embed"
                             width="100%"
                             height="350"
                             style="border:0;"
